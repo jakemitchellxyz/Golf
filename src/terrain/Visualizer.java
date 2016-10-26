01e5b1806f7eac76aa77540dfb8f73d28063edb2
Koushul <Koushul@10.5.16.252>
2016-10-26 14:13:04 -0400

    Visualizer

--- src/terrain/Visualizer.java
+++ src/terrain/Visualizer.java
@@ -4,7 +4,7 @@ import physics.Ball;
 
 // https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Peripheral_vision.svg/2000px-Peripheral_vision.svg.png
 
-public class Visualizer {
+public class Visualizer { // Allow user to look around
 	
 	private double[] obstacles; 
 		
